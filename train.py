bd834b6e6e82045fb26b56413336bbf1cd9bc07c
Muralidharan Raju <110589056+MuralisGit@users.noreply.github.com>
2026-03-12 14:19:41 -0400

    Add median_absolute_error to import statements

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import time
 
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, median_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 
